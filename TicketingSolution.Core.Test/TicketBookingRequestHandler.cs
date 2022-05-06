2f80805e848d918b53eff82f3c410fe26b12bba0
hamed <hamed@DESKTOP-K8GBLKF>
2022-05-07 04:13:36 +0430

    added firs test added shouldly

---
+++ TicketingSolution.Core.Test/TicketBookingRequestHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TicketingSolution.Core
+{
+    public class TicketBookingRequestHandler
+    {
+        public TicketBookingRequestHandler()
+        {
+        }
+
+        internal ServiceBookingResult BookService(TicketBookingRequest request)
+        {
+            ServiceBookingResult result = new ServiceBookingResult();   
+
+            result.Email = request.Email;
+            result.Name = request.Name; 
+            result.Family=request.Family;   
+
+            return result;
+        }
+    }
+}
---
+++ TicketingSolution.Core.Test/TicketBookingRequestHandlerTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+using Shouldly;
+
+namespace TicketingSolution.Core
+{
+    public class Ticket_Booking_Request_Handler_Test
+    {
+        [Fact]
+        public void Should_Return_Ticket_Booking_Response_With_Request_Value()
+        {
+            //Arrange
+
+            var BookingRequest = new TicketBookingRequest() 
+            {
+                Name="test Name",
+                Family="test Family",
+                Email="test Email",
+            };
+
+            var Handler = new TicketBookingRequestHandler();
+
+
+            //Act
+            ServiceBookingResult result= Handler.BookService(BookingRequest);
+
+
+            //Ordinary Assertation
+
+            //Assert.NotNull(result);
+            //Assert.Equal(result.Name,BookingRequest.Name);
+            //Assert.Equal(result.Family,BookingRequest.Family);
+            //Assert.Equal(result.Email,BookingRequest.Email);
+
+
+            //Shouldly Assertaion
+            result.ShouldNotBeNull();
+            result.Name.ShouldBe(BookingRequest.Name);
+            result.Family.ShouldBe(BookingRequest.Family);  
+            result.Email.ShouldBe(BookingRequest.Email);    
+
+
+
+
+        }
+    }
+}
